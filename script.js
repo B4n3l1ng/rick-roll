53d72aa862e027279bc31fd2326bdd1373c54e10
Eric <eric.coimbra@gmail.com>
2023-08-11 10:38:25 +0100

    volume bar added

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 window.addEventListener("load", () => {
   const rick = new Audio("./assets/rick-roll.mp3");
   rick.volume = 0.7;
+  const slider = document.querySelector("#volume");
+  slider.addEventListener("change", (event) => {
+    console.log(parseInt(event.target.value) / 10);
+    rick.volume = parseInt(event.target.value) / 10;
+  });
   const body = document.querySelector("#body");
   body.addEventListener("click", () => {
     rick.play();
