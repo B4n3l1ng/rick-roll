e8766f16b789d1ffd015a0b4d1037bc33af8cf5e
Eric <eric.coimbra@gmail.com>
2023-08-11 10:48:28 +0100

    fixed it Mat, happy?

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ window.addEventListener("load", () => {
   rick.volume = 0.7;
   const slider = document.querySelector("#volume");
   slider.addEventListener("change", (event) => {
-    console.log(parseInt(event.target.value) / 10);
-    rick.volume = parseInt(event.target.value) / 10;
+    console.log(parseInt(event.target.value) / 100);
+    rick.volume = parseInt(event.target.value) / 100;
   });
   const body = document.querySelector("#body");
   body.addEventListener("click", () => {
